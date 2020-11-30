e893911b0c1fc728924a73f077e51c8b177bd8a7
Michael Lackey <michael.g.lackey@gmail.com>
2020-11-30 13:56:57 -0600

    adjust font sizing

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,8 +4,9 @@ import React from 'react';
 const HomePage = (props) => {
   return (
     <div className="HomePage">
-        <header className="home-heading">Welcome to Ranger Builder</header>
+        <header className="home-heading">Ranger Builder</header>
         <p>A warband building app for Rangers of the Shadow Deep</p>
+          <img className="home-img" alt="cover" src="https://i.imgur.com/dUvbVJZ.jpg"></img>
     </div>
   )
 }
