c6ff7ee70165f5849c72b6fe7bde67d055625f64
Michael Lackey <michael.g.lackey@gmail.com>
2020-12-06 13:31:25 -0600

    add reference link boxes

--- src/pages/ReferencesPage/ReferencesPage.jsx
+++ src/pages/ReferencesPage/ReferencesPage.jsx
@@ -10,25 +10,24 @@ const ReferencesPage = () => {
         <div className="references-container">
           <img className="references-img" alt="ranger" src="https://i.imgur.com/4t4IFi2.jpg"></img>
           <div className="references-links">
-            <Link to='/references/map' className='reference'>Map</Link>
-            &nbsp;&nbsp;&nbsp;
-            <Link to='/references/bestiary' className='reference'>Bestiary</Link>
-            &nbsp;&nbsp;&nbsp;
-            <Link to='/references/endofgame' className='reference'>End of Game</Link>
-            &nbsp;&nbsp;&nbsp;
-            <Link to='/references/abilities' className='reference'>Heroic Abilities</Link>
-            &nbsp;&nbsp;&nbsp;
-            <Link to='/references/spells' className='reference'>Spells</Link>
-            &nbsp;&nbsp;&nbsp;
-            <Link to='/references/skills' className='reference'>Skills</Link>
-            &nbsp;&nbsp;&nbsp;
-            <Link to='/references/equipment' className='reference'>Equipment</Link>
-            &nbsp;&nbsp;&nbsp;
-            <Link to='/references/herbsandpotions' className='reference'>Herbs & Potions</Link>
-            &nbsp;&nbsp;&nbsp;
-            <Link to='/references/weaponsandarmour' className='reference'>Weapons & Armour</Link>
-            &nbsp;&nbsp;&nbsp;
-            <Link to='/references/magicitems' className='reference'>Magic Items</Link>
+            <div className="link-row">
+              <Link to='/references/map' className="link-box">Map</Link>
+              <Link to='/references/bestiary' className='link-box'>Bestiary</Link>
+              <Link to='/references/endofgame' className='link-box'>End of Game</Link>
+            </div>
+            <div className="link-row">
+              <Link to='/references/abilities' className='link-box'>Heroic Abilities</Link>
+              <Link to='/references/spells' className='link-box'>Spells</Link>
+              <Link to='/references/skills' className='link-box'>Skills</Link>
+            </div>
+            <div className="link-row">
+              <Link to='/references/equipment' className='link-box'>Equipment</Link>
+              <Link to='/references/herbsandpotions' className='link-box'>Herbs & Potions</Link>
+              <Link to='/references/weaponsandarmour' className='link-box'>Weapons & Armour</Link>
+            </div>
+            <div className="link-row">
+              <Link to='/references/magicitems' className='link-box'>Magic Items</Link>
+            </div>
           </div>
         </div>
     </div>
